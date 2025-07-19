eebce1f395675f98c619b1003d7738c401ac1911
David <oscardavidestradabetancourth@gmail.com>
2025-07-19 09:17:37 -0500

    primer intento para backend en vercel, añadiendo un cron usandog github actions para supabase

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-export const loginRequest = (user) => axios.post(import.meta.env.VITE_API_BACKEND_LOGIN, user)
+export const loginRequest = (user) => axios.post(`${import.meta.env.VITE_API_BACKEND_URL}/login`, user)
 
-export const registerRequest = (user) => axios.post(import.meta.env.VITE_API_BACKEND_REGISTER, user)
+export const registerRequest = (user) => axios.post(`${import.meta.env.VITE_API_BACKEND_URL}/register`, user)
 
 
--- client/src/api/rifa.js
+++ client/src/api/rifa.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
-export const getRifas = (token) => axios.post(import.meta.env.VITE_API_BACKEND_GET_RIFAS, {token} );
-export const createRifa = (rifa) => axios.post(import.meta.env.VITE_API_BACKEND_CREATE_RIFA, rifa);
-export const getAllRifas = () => axios.get(import.meta.env.VITE_API_BACKEND_GET_ALL_RIFAS);
-export const getRifaById = (rifaId) => axios.get(import.meta.env.VITE_API_GET_RIFA, {params : {rifaId}});
-export const updatePartialRifa = (id, fieldsToUpdate) => axios.patch(import.meta.env.VITE_UPDATE_RIFA, fieldsToUpdate, {params : {id}});
-export const decrementRifaTicket = (rifaId, amount) => axios.patch(import.meta.env.VITE_DECREMENT_TICKET, {rifaId, amount});
+export const getRifas = (token) => 
+    axios.post(`${import.meta.env.VITE_API_BACKEND_URL}/create/rifa`, {token} );
+export const createRifa = (rifa) => 
+    axios.post(`${import.meta.env.VITE_API_BACKEND_URL}/create/rifa`, rifa);
+export const getAllRifas = () => 
+    axios.get(`${import.meta.env.VITE_API_BACKEND_URL}/get/all/rifas`);
+export const getRifaById = (rifaId) => 
+    axios.get(`${import.meta.env.VITE_API_BACKEND_URL}/get/rifa`, {params : {rifaId}});
+export const updatePartialRifa = (id, fieldsToUpdate) => 
+    axios.patch(`${import.meta.env.VITE_API_BACKEND_URL}/update/rifa`, fieldsToUpdate, {params : {id}});
+export const decrementRifaTicket = (rifaId, amount) => 
+    axios.patch(`${import.meta.env.VITE_API_BACKEND_URL}/decrement/ticket`, {rifaId, amount});
 export const checkRifaForDraw = (rifaId, token) => 
     axios.post(`${import.meta.env.VITE_API_BACKEND_URL}/rifas/${rifaId}/check-draw`, { token });
 export const realizarSorteo = (rifaId, token) => 
